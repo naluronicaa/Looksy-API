ca288683d56950f86b3660cfe9246ceb338d34aa
naluronicaa <luchan.217.silva@gmail.com>
2025-05-22 15:39:31 -0300

    alteração para aws

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ app.register_blueprint(questionario_bp)
 app.register_blueprint(usuarios_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
