518284a67e64a0514061273e3435a8e82cab154c
naluronicaa <luchan.217.silva@gmail.com>
2025-06-01 13:28:26 -0300

    adicição de cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS  # 👈 importa o CORS
 from routes.roupas_routes import roupas_bp
 from routes.questionario_routes import questionario_bp
 from routes.usuarios_routes import usuarios_bp
@@ -6,6 +7,10 @@ from routes.usuarios_routes import usuarios_bp
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+# Habilita CORS para todas as rotas e origens (para testes locais)
+CORS(app)
+
+# Registra as rotas
 app.register_blueprint(roupas_bp)
 app.register_blueprint(questionario_bp)
 app.register_blueprint(usuarios_bp)
